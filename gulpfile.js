07b5b112277ff8769f0dd19fa25cca37b8a51e2b
Chris Feragotti <chrisferagotti@gmail.com>
2019-02-17 22:40:26 -0500

    CF: Update scss files and gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ var sequence = require('gulp-sequence');
 var uglify = require('gulp-uglify');
 var cleanCss = require('gulp-clean-css');
 var concat = require('gulp-concat');
-var sourcemaps = require('gulp-sourcemaps');
+var sourceMaps = require('gulp-sourcemaps');
 var rename = require('gulp-rename');
 var clean = require('gulp-clean');
 
@@ -27,8 +27,8 @@ gulp.task('sass', function () {
     gulp.src(theme_location + 'dist/css', {read: false})
         .pipe(clean());
     return gulp.src(theme_location + 'src/scss/layout.scss')
-        .pipe(sass())
-        .pipe(concat('styles.css'))
+        .pipe(sass())                        // Compile SASS
+        .pipe(concat('style.css'))
         .pipe(gulp.dest(theme_location + 'dist/css'))
 });
 
@@ -38,33 +38,45 @@ gulp.task('sass', function () {
 // });
 
 // CSS Minify
-// gulp.task('cssmin', function(){
-//     return gulp.src(theme_location + 'dist/css/style.css')
-//         .pipe(cleanCss())
-//         .pipe(rename({suffix: '.min'}))
-//         .pipe(gulp.dest(theme_location + 'dist/css'))
-// });
+gulp.task('cssmin', function() {
+    return gulp.src(theme_location + 'dist/css/style.css')
+        .pipe(cleanCss())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(theme_location + 'dist/css'))
+});
 
 // JS Concat
-// gulp.task('jscon', function(){
-//     return gulp.src(theme_location + 'js/*.js')
-//         .pipe(concat('all.js'))
-//         .pipe(gulp.dest(theme_location + 'dist/js'))
-// });
+gulp.task('jscon', function(){
+    return gulp.src(theme_location + 'src/js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest(theme_location + 'dist/js'))
+});
 
 // JS Minify
-// gulp.task('jsmin', function(){
-//     return gulp.src(theme_location + 'dist/js/all.js')
-//         .pipe(uglify())
-//         .pipe(rename({suffix: '.min'}))
-//         .pipe(gulp.dest(theme_location + 'dist/js'))
-// });
+gulp.task('jsmin', function(){
+    return gulp.src(theme_location + 'dist/js/all.js')
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(theme_location + 'dist/js'))
+});
+
+
+// SASS
+gulp.task('img', function () {
+    gulp.src(theme_location + 'dist/images', {read: false})
+        .pipe(clean());
+    return gulp.src(theme_location + 'src/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest(theme_location + 'dist/images'))
+});
 
 // ----Multi Tasks----
 // SASS + CSS Minify
 gulp.task('sassmin', sequence('sass', 'cssmin'))
 // JS Concat + JS Uglify
 gulp.task('jsconmin', sequence('jscon', 'jsmin'))
+// Imaages
+gulp.task('images', sequence('img'))
 
 // Default - SASS + CSS Minify + JS Concat + JS Uglify
-gulp.task('default', sequence('sass', 'cssmin', 'jscon', 'jsmin'))
+gulp.task('default', sequence('sass', 'cssmin', 'jscon', 'jsmin', 'img'))
