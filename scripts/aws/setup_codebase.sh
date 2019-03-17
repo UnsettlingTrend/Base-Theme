c5df6f06dc2eec67f9688d7993475e30f600680b
Chris Feragotti <chrisferagotti@gmail.com>
2019-03-17 11:36:37 -0400

    CF: Updating buldspec / appspec.

---
+++ scripts/aws/setup_codebase.sh
@@ -0,0 +1,9 @@
+cd /var/www
+
+# Delete all directories and files (except for build.tar)
+ls | grep -v build.tar | xargs rm
+rm -rf ./*/
+
+# Untar the build file
+sudo tar -xf build.tar
+mv /var/www/web /var/www/html
