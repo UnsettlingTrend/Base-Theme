1938c80ced48a933d80e0fe22a8a357031a5fb4a
Chris Feragotti <chrisferagotti@gmail.com>
2019-03-17 20:16:23 -0400

    CF: Updating buldspec / appspec.

--- scripts/aws/02_install_codebase.sh
+++ scripts/aws/02_install_codebase.sh
@@ -1,12 +1,11 @@
 sudo mkdir -p /var/www
 cd /var/www
 
-# Delete all directories and files (except for build.tar)
-sudo ls | grep -v build.tar | xargs rm
-sudo rm -rf ./*/
+# Delete all directories and files
+rm -rf  ./
 
 # Untar the build file
-sudo tar -xf ~/build.tar
+sudo tar -xf /tmp/build.tar
 sudo mv /var/www/web /var/www/html
 
 
