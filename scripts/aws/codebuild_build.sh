f3583fe2fbb6e2ebc4343f51e30d242dbd049613
Chris Feragotti <chrisferagotti@gmail.com>
2021-05-15 16:24:50 -0400

    CF: Refactor build scripts.

--- scripts/aws/codebuild_build.sh
+++ scripts/aws/codebuild_build.sh
@@ -4,12 +4,12 @@ set -e
 #composer -vvv about
 #ls /etc/php/
 #php -v
-#echo Build started on `date`
-#echo Installing composer deps
-#composer install --no-progress --no-suggest
-#npm -v
-#echo Installing nodejs deps
-#npm install
+pwd
+echo Build started on `date`
+echo Installing composer packages...
+composer install --no-progress --no-suggest
+echo Installing nodejs packages...
+npm install
 # Do you need to do this? In many cases phpunit will use sqllite or similar to avoid the need for a real DB.
 # If you don't need it delete it
 #/usr/bin/mysql  -u root -e "GRANT ALL ON *.* TO 'test'@'localhost' IDENTIFIED BY '' WITH GRANT OPTION"
