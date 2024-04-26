6ef00fca3a700ea2576f78e5009c2f6b13efe3fe
Chris Feragotti <chrisferagotti@gmail.com>
2024-04-26 08:11:13 -0400

    CF: Update stuff.

--- web/themes/custom/ut_base/gulpfile.js
+++ web/themes/custom/ut_base/gulpfile.js
@@ -1,21 +1,49 @@
-'use strict';
-const { src, dest } = require('gulp');
-const gulp_sass = require('gulp-sass')(require('sass'));
-const gulp_minify = require('gulp-minify');
+const { gulp, src, dest, series, parallel } = require('gulp');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+const imagemin = import('gulp-imagemin');
+const ttf2woff2 = require('gulp-ttf2woff2');
 
-function css(done) {
-  src('src/scss/style.scss')
-    .pipe(gulp_sass().on('error', gulp_sass.logError))
-    .pipe(dest('dist/css'));
-  done();
+// Paths
+const jsSrc = 'src/js/*.js';
+const jsDest = 'dist/js/';
+const scssSrc = 'src/scss/*.scss';
+const cssDest = 'dist/css/';
+const fontsSrc = 'src/fonts/*';
+const fontsDest = 'dist/fonts/';
+
+// Sass task: compile to CSS and minify
+function css() {
+  return src(scssSrc)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('styles.css'))
+    .pipe(cleanCSS())
+    .pipe(dest(cssDest));
 }
 
-function js(done) {
-  src(['src/js/*.js', 'src/js/*.mjs'])
-    .pipe(gulp_minify())
-    .pipe(dest('dist/js'));
-  done();
+// JavaScript task: concatenate and minify
+function js() {
+  return src(jsSrc)
+    .pipe(concat('scripts.js'))
+    .pipe(uglify())
+    .pipe(dest(jsDest));
 }
 
-exports.css = css
-exports.js = js
+// Convert TTF to WOFF2
+function convertTTFtoWOFF2() {
+  return src('src/fonts/*.ttf')
+    .pipe(ttf2woff2())
+    .pipe(dest('src/fonts/'));
+}
+
+// Minify font files
+function compressFonts() {
+  return src('src/fonts/*.{woff,woff2}')
+    .pipe(dest(fontsDest));
+}
+
+// Default task
+exports.default = series(parallel(js, css, convertTTFtoWOFF2, compressFonts));
+
