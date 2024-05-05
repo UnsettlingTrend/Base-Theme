46342c445ad9032a8016a3f9325733b3eaf05a56
Chris Feragotti <chrisferagotti@gmail.com>
2024-05-04 22:00:45 -0400

    CF: Some project cleanup.

--- lando/scripts/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Run all the build files, alphabetically
-for filename in /app/lando/scripts/build_[0-9][0-9]_*.sh; do
-    [ -e "$filename" ] || continue  # Check for no-match / glob case
-    bash $filename;
-done
--- lando/scripts/build_02_install_dependencies.sh
+++
@@ -1,4 +0,0 @@
-apt-get install python3-pip
-pip3 install robin_stocks pyyaml
-
-apt-get update -y
